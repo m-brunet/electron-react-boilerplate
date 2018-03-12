ff33f5a4fb06919fbd3281950629e950b851df31
Matthias Brunet <matthias.brunet@viseo.com>
2018-03-12 13:40:24 +0100

    modified boilerplate

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,9 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import App from './containers/app.container';
+import HomePage from './containers/home-page.container';
+import CounterPage from './containers/counter-page.container';
 
 export default () => (
   <App>
